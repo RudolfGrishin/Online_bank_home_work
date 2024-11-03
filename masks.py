007483b4563016706e0876a0d0be7ae28c50096b
Rudolf.Grishin <sumlyaninov74@mail.ru>
2024-11-03 17:55:32 +0300

    add masks.py

--- masks.py
+++ masks.py
@@ -4,7 +4,7 @@ def get_mask_card_number(card_number: str) -> str:
         raise ValueError("Неверная длина номера карты")
     if not card_number.isdigit():
         raise ValueError("Номер должен состоять только из цифр")
-    return f"{card_number[0:4]} {card_number[4:6]} ** {card_number[12:16]}"
+    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}"
 
 
 def get_mask_account(account_number: str) -> str:
@@ -13,4 +13,4 @@ def get_mask_account(account_number: str) -> str:
         raise ValueError("Неверная длина номера счёта")
     if not account_number.isdigit():
         raise ValueError("Номер счёта должен состоять только из цифр")
-    return f"**{account_number[-5:-1]}"
+    return f"**{account_number[-4:]}"
