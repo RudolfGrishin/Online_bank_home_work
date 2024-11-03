d60c810e43a9d23384c820b28ed194a8c97b179b
Rudolf.Grishin <sumlyaninov74@mail.ru>
2024-11-03 07:53:38 +0300

    example.py

--- example.py
+++ example.py
@@ -6,6 +6,7 @@ def get_mask_card_number(card_number: str) -> str:
     raise ValueError("Номер должен состоять только из цифр")
   return f"{card_number[0:4]} {card_number[4:6]} ** {card_number[12:16]}"
 
+
 def get_mask_account(account_number: str) -> str:
   """Функция маскирует номер принимаемого счёта"""
   if len(account_number) != 20:
