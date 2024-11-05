b47e3eaac9cdd2a00f2d45deaadbfc0e4eb829f2
Rudolf.Grishin <sumlyaninov74@mail.ru>
2024-11-05 14:31:41 +0300

    add widget.py

--- src/widget.py
+++ src/widget.py
@@ -3,13 +3,14 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(number: str) -> str:
     """Функция маскировки общих данных пользователя карты и номера счёта."""
-
-    if "Счёт" in number:
-        return get_mask_account(number)
-    else:
-        cards = get_mask_card_number(number[-16:])
-        new_card = number.replace(number[-16:], cards)
-        return new_card
+    original_number = number.split()[-1]
+    if len(original_number) == 16:
+        mask_number_1 = get_mask_card_number(original_number)
+        result = f"{number[:-16]}{mask_number_1}"
+    elif len(original_number) == 20:
+        mask_number_2 = get_mask_account(original_number)
+        result = f"{number[:-20]}{mask_number_2}"
+    return result
 
 
 def get_date(old_data: str) -> str:
