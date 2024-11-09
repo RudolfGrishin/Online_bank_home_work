d4d41dc095d48d1b2a0992bcc6e4700837d3d82e
Rudolf.Grishin <sumlyaninov74@mail.ru>
2024-11-09 12:40:55 +0300

    add processing.py

--- src/processing.py
+++ src/processing.py
@@ -10,7 +10,12 @@ list_of_dicts = [
 
 
 def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
-    """
-    Фильтрует список словарей по значению ключа 'state'
-    """
+    """Фильтрует список словарей по значению ключа 'state'"""
+
     return [item for item in data if item.get('state') == state]
+
+
+def sort_by_date(data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
+    """Сортирует список словарей по значению ключа 'date'."""
+
+    return sorted(data, key=lambda x: x['date'], reverse=reverse)
